eab5ec1a9e8e183d7cda67d211b0478591e55f2b
sebastianlewandowski <seb_lew@live.se>
2017-10-26 16:13:30 +0200

    Complete board layout
    Chessboard

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-
+var nrOfSquares=8;
 var contentDiv=document.getElementById("content");
 
 var board=document.createElement("div");
 board.className="board";
 
-for(var y = 0; y < 8; y++){
 
-var row= document.createElement("div")
-row.className="row";
-for(var y = 0; y < 8; y++){
-    addRowToBoard();
+for(var y = 0; y < nrOfSquares; y++){
+  var row =  addRowToBoard();
+for(var x=0; x<nrOfSquares; x++){
+    var square= document.createElement("div");
+    square.className="square";
+    if((x+y)%2){
+        square.style.backgroundColor="black"
+        }else{
+        square.style.backgroundColor="white"
+        }
+    row.appendChild(square);
 }
-if(y%2){
-row.style.backgroundColor="black"
-}else{
-row.style.backgroundColor="white"
 }
 
-
-
-board.appendChild(row);
-}
 contentDiv.appendChild(board);
 
 function addRowToBoard(){
@@ -34,8 +32,9 @@ function addRowToBoard(){
         }else{
         row.style.backgroundColor="white"
         }
-        
-        
+
         
         board.appendChild(row);
+        return row;
 }
+
